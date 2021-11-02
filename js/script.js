e07843730bc43dac14bb1014926bf5c6c39a9634
Ashkan Zarifian <zs.ashkan@yahoo.com>
2021-11-03 01:02:22 +0300

    Fix some responsive prolems

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
     easing: "cubic-bezier(0.25, 0, 0.25, 1)",
     speed: 500,
     pauseOnHover: true,
-    auto: true,
     loop: true,
+    auto: true,
     slideEndAnimation: true,
     pause: 2000,
     responsive: [
@@ -28,6 +28,40 @@ $(document).ready(function () {
         breakpoint: 600,
         settings: {
           item: 1,
+          slideMove: 1,
+        },
+      },
+    ],
+  });
+  $("#profile_slider").lightSlider({
+    pager: false,
+    item: 4,
+    slideMargin: 0,
+    slideMove: 2,
+    easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+    speed: 500,
+    pauseOnHover: true,
+    auto: true,
+    loop: true,
+    slideEndAnimation: true,
+    pause: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          item: 4,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          item: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          item: 2,
           slideMove: 1,
         },
       },
